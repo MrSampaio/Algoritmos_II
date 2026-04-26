51697c0871772af12aa7457b41eab30156372b3a
MrSampaio <jusampa2@gmail.com>
2026-04-26 15:42:26 -0300

    arrays

---
+++ src/Arrays/ExibirArray.java
@@ -0,0 +1,11 @@
+package Arrays;
+import java.util.Arrays;
+
+public class ExibirArray {
+    public static void main(String[] args){
+        int[] listaNumeros = {5, 12, 2, 6, 40};
+
+        System.out.println(Arrays.toString(listaNumeros));
+
+    }
+}
